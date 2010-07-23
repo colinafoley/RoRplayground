4d13cc7a7af69cd30b5da4506637332f13fc9819
Colin A Foley <colin.a.foley@gmail.com>
2010-07-23 16:46:32 -0400

    Added appointment-address association

--- pim/app/models/appointment.rb
+++ pim/app/models/appointment.rb
@@ -1,3 +1,7 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :name, :start
+  validates_length_of :name, :minimum => 3
+  
+  has_one :appointment_address
 end
