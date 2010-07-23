ce0f1157ecf65b12d0f158a24ddd7d3f69b3d66d
Colin A Foley <colin.a.foley@gmail.com>
2010-07-22 22:53:26 -0400

    Add individual phones

--- pim/db/schema.rb
+++ pim/db/schema.rb
@@ -9,13 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100723022659) do
+ActiveRecord::Schema.define(:version => 20100723025227) do
 
   create_table "contacts", :force => true do |t|
     t.string   "c_fname"
     t.string   "c_mname"
     t.string   "c_lname"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "phones", :force => true do |t|
+    t.string   "name"
+    t.string   "number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
