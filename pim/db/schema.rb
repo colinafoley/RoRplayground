d6630c40b058227715b8aeefd5906086e84a5084
Colin A Foley <colin.a.foley@gmail.com>
2010-07-22 22:29:10 -0400

    Contacts table added to DB

--- pim/db/schema.rb
+++ pim/db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100723020942) do
+ActiveRecord::Schema.define(:version => 20100723022659) do
+
+  create_table "contacts", :force => true do |t|
+    t.string   "c_fname"
+    t.string   "c_mname"
+    t.string   "c_lname"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "uname"
