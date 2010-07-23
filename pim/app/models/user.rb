623d7a6235ec5328549dd295b7525e03deb4ef44
Colin A Foley <colin.a.foley@gmail.com>
2010-07-22 22:35:53 -0400

    Added user validation, contacts association

--- pim/app/models/user.rb
+++ pim/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates_presence_of :uname, :password
+  validates_length_of :uname, :minimum => 4
+  validates_length_of :password, :minimum => 5
+
+  has_many :contacts
 end
