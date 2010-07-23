3c18c521ab450479332cdf39f4fc75bed8b7831c
Colin A Foley <colin.a.foley@gmail.com>
2010-07-22 22:57:11 -0400

    Added phone-contact association, phone validation

--- pim/app/models/contact.rb
+++ pim/app/models/contact.rb
@@ -2,4 +2,6 @@ class Contact < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :c_fname
   validates_length_of :c_fname, :minimum => 3
+
+  has_many :phones
 end
--- pim/app/models/phone.rb
+++ pim/app/models/phone.rb
@@ -1,2 +1,6 @@
 class Phone < ActiveRecord::Base
+  belongs_to :contact
+
+  validate_presence_of :name, :number
+  validate_length_of :name, :minimum => 1
 end
