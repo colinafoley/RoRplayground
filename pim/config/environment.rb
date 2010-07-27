d9633f1f0c7ef7982201221710aa713e72807966
Colin A Foley <colin.a.foley@gmail.com>
2010-07-27 14:53:11 -0400

    Installed Devise

--- pim/config/environment.rb
+++ pim/config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml", :version => ">=3.0.13"
+  config.gem "warden"
+  config.gem "devise"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,4 +41,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
